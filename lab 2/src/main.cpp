2c48bb56243edbc3cc3a734f1ad26467b9dad437
Ijeoma Arisa <iarisa@uci.edu>
2024-11-08 15:38:33 -0800

    Final submission

--- lab 2/src/main.cpp
+++ lab 2/src/main.cpp
@@ -16,7 +16,7 @@
 
 int tl_state; // Traffic light state.
 unsigned long tl_timer; // Traffic light timer.
-int buttonState = 0;
+int buttonState;  // Button state.
 
 void setup(){
 // Configure LED pins as outputs.
@@ -25,32 +25,23 @@ void setup(){
   pinMode(GREEN_PIN, OUTPUT);
   pinMode(BUZZER_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT_PULLUP);
-  Serial.begin(9600);
   // Initial state for states and timers..
   tl_state = RED_STATE;
   tl_timer = millis() + RED_MILLIS;
 }
 void loop(){
   buttonState = digitalRead(BUTTON_PIN);
-  Serial.println(buttonState);
-  // tone(BUZZER_PIN, 440);
-  // if (buttonState == HIGH){
-    
-  //   digitalWrite(GREEN_PIN, LOW);
-  //   digitalWrite(RED_PIN, LOW);
-  //   digitalWrite(YELLOW_PIN, LOW);
-  // }
-  // if (buttonState == LOW){
-  //   // tone(BUZZER_PIN, 440);
-  //   digitalWrite(GREEN_PIN, HIGH);
-  //   digitalWrite(RED_PIN, HIGH);
-  //   digitalWrite(YELLOW_PIN, HIGH);
-  // }
+
   // TL state machine
   switch (tl_state){
     case RED_STATE:
     // Turn red light on.
     digitalWrite(RED_PIN, HIGH);
+    // Activate buzzer.
+    tone(BUZZER_PIN, 440);
+    delay(250);
+    noTone(BUZZER_PIN);
+    delay(250);
 
     if (millis() >= tl_timer){
       // Turn red light off.
@@ -61,16 +52,6 @@ void loop(){
     }
     break;
     
-    case YELLOW_STATE:
-    // Code for yellow state.
-    digitalWrite(YELLOW_PIN, HIGH);
-
-     if (millis() >= tl_timer){
-      digitalWrite(YELLOW_PIN, LOW);
-      tl_state = GREEN_STATE;
-      tl_timer = millis() + GREEN_MILLIS;
-    }
-    break;
     case RED_YELLOW_STATE:
     // Code for red-yellow state.
     digitalWrite(RED_PIN, HIGH);
@@ -82,67 +63,38 @@ void loop(){
       tl_state = GREEN_STATE;
       tl_timer = millis() + GREEN_MILLIS;
     }
+    break;
+
+    case YELLOW_STATE:
+    // Code for yellow state.
+    digitalWrite(YELLOW_PIN, HIGH);
+
+     if (millis() >= tl_timer){
+      digitalWrite(YELLOW_PIN, LOW);
+      tl_state = RED_STATE;
+      tl_timer = millis() + RED_MILLIS;
+    }
+    break;
+
     case GREEN_STATE:
-    // Turn green light on.
-    
+    // Turn green light on. 
     digitalWrite(GREEN_PIN, HIGH);
 
-    /* EDIT THIS*/
-    // if (tl_timer % GREEN_MILLIS == 0 /*Timer expired AND touch-button was pressed*/){
-    //   // Turn green light off.
-    //   digitalWrite(GREEN_PIN, LOW);
+    // Activate buzzer
+    tone(BUZZER_PIN, 523);
+    delay(500);
+    noTone(BUZZER_PIN);
+    delay(1500);
 
-    //   // Set timer for yellow state.
-    //   tl_state = YELLOW_STATE;
-    //   tl_timer = millis() + YELLOW_MILLIS;
-
-    // }
     if (buttonState == LOW){
-      tl_state = YELLOW_STATE;
-      delay(5000);
-    }
-    break;
-  }
-  // Detect touch - button pressed.
-  // Buzzer state machine.
-  // .
-  // .
-  switch (tl_state){
-    case RED_STATE:
-      tone(BUZZER_PIN, 440);
-      delay(250);
-      noTone(BUZZER_PIN);
-      delay(250);
-    break;
-    case RED_YELLOW_STATE:
-    // Code for red-yellow state.
-    digitalWrite(RED_PIN, HIGH);
-    digitalWrite(YELLOW_PIN, HIGH);
-
-    if (millis() >= tl_timer){
-      digitalWrite(RED_PIN, LOW);
-      digitalWrite(YELLOW_PIN, LOW);
-      tl_state = GREEN_STATE;
-      tl_timer = millis() + GREEN_MILLIS;
-    }
-    break;
-    case YELLOW_STATE:
-    // Code for yellow state.
-    digitalWrite(YELLOW_PIN, HIGH);
-
-     if (millis() >= tl_timer){
-      digitalWrite(YELLOW_PIN, LOW);
-      tl_state = GREEN_STATE;
-      tl_timer = millis() + GREEN_MILLIS;
-    }
-    break;
-    case GREEN_STATE:
-      Serial.println("Playing green State");
-      tone(BUZZER_PIN, 523);
       delay(500);
-      noTone(BUZZER_PIN);
-      delay(1500);
-    
-    break;
+      // Turn green light off.
+      digitalWrite(GREEN_PIN, LOW);
+
+      // Set timer for yellow state.
+      tl_state = YELLOW_STATE;
+      tl_timer = millis() + YELLOW_MILLIS;
+
+    }
   }
 }
