902bfcaec036a77d83e0bcdc03db6671916c31c1
Ijeoma Arisa <iarisa@uci.edu>
2024-11-08 13:41:55 -0800

    Updated red, red-yellow states. Working on green state.

--- lab 2/src/main.cpp
+++ lab 2/src/main.cpp
@@ -11,10 +11,13 @@
 #define YELLOW_STATE 2
 #define GREEN_STATE 3
 #define RED_MILLIS 10000
+#define YELLOW_MILLIS 2000
+#define GREEN_MILLIS 5000
 
 int tl_state; // Traffic light state.
 unsigned long tl_timer; // Traffic light timer.
 int buttonState = 0;
+
 void setup(){
 // Configure LED pins as outputs.
   pinMode(RED_PIN, OUTPUT);
@@ -46,25 +49,43 @@ void loop(){
     case RED_STATE:
     // Turn red light on.
     digitalWrite(RED_PIN, HIGH);
-    if (/*Timer expired*/){
+
+    if (millis() >= tl_timer){
       // Turn red light off.
       digitalWrite(RED_PIN, LOW);
       // Set timer for red-yellow state.
       tl_state = RED_YELLOW_STATE;
+      tl_timer = millis() + YELLOW_MILLIS;
     }
     break;
     case RED_YELLOW_STATE:
     // Code for red-yellow state.
+    digitalWrite(RED_PIN, HIGH);
+    digitalWrite(YELLOW_PIN, HIGH);
+
+    if (millis() >= tl_timer){
+      digitalWrite(RED_PIN, LOW);
+      digitalWrite(YELLOW_PIN, LOW);
+      tl_state = GREEN_STATE;
+      tl_timer = millis() + GREEN_MILLIS;
+    }
     break;
     case YELLOW_STATE:
     // Code for yellow state.
     break;
     case GREEN_STATE:
     // Turn green light on.
-    if (/*Timer expired AND touch-button was pressed*/){
+    digitalWrite(GREEN_PIN, HIGH);
+
+    /* EDIT THIS*/
+    if (tl_timer % GREEN_MILLIS == 0 /*Timer expired AND touch-button was pressed*/){
       // Turn green light off.
-      // Set timer for yellow
-      state.tl_state = YELLOW_STATE;
+      digitalWrite(GREEN_PIN, LOW);
+
+      // Set timer for yellow state.
+      tl_state = YELLOW_STATE;
+      tl_timer = millis() + YELLOW_MILLIS;
+
     }
     break;
   }
