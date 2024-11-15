f79a1b2c24fa3fbecd233d6c7225be11fc8ecd64
aedwardz <antonile@uci.edu>
2024-11-15 10:31:07 -0800

    starter

--- Lab 3/src/main.cpp
+++ Lab 3/src/main.cpp
@@ -1,12 +1,51 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <SPI.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+
+#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 
 
+class MyCallbacks: public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) {
+  std::string value = pCharacteristic->getValue();
+  if (value.length() > 0) {
+    Serial.println("*********");
+    Serial.print("New value: ");
+    for (int i = 0; i < value.length(); i++)
+      Serial.print(value[i]);
+      Serial.println();
+      Serial.println("*********");
+  }
+  }
+};
 void setup() {
-  // put your setup code here, to run once:
-}
+  Serial.begin(9600);
+  Serial.println("1- Download and install an BLE scanner app in your phone");
+  Serial.println("2- Scan for BLE devices in the app");
+  Serial.println("3- Connect to MyESP32");
+  Serial.println("4- Go to CUSTOM CHARACTERISTIC in CUSTOM SERVICE and write something");
+  Serial.println("5- See the magic =)");
+  BLEDevice::init("MyESP32-group 59");
+  BLEServer *pServer = BLEDevice::createServer();
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
+  CHARACTERISTIC_UUID,
+  BLECharacteristic::PROPERTY_READ |
+  BLECharacteristic::PROPERTY_WRITE
+);
+pCharacteristic->setCallbacks(new MyCallbacks());
+pCharacteristic->setValue("Hello World");
 
+pService->start();
+BLEAdvertising *pAdvertising = pServer->getAdvertising();
+pAdvertising->start();
+}
 void loop() {
-  // put your main code here, to run repeatedly:
+
+
+  delay(2000);
 }
