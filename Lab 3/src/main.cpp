073e9acf919f5c00db3d375c44fdcc4a8121a69a
aedwardz <antonile@uci.edu>
2024-11-21 18:46:58 -0800

    lab 4 template

--- Lab 3/src/main.cpp
+++ Lab 3/src/main.cpp
@@ -4,11 +4,20 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
+#include "SparkFunLSM6DSO.h"
+#include <limits>
+#include <string>
 
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 #define SERVICE_UUID "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
 
 int ledPin = 25;
+LSM6DSO myIMU;
+float x;
+float y;
+float threshold;
+int steps = 0;
+
 
 BLECharacteristic *pCharacteristic;
 
@@ -55,10 +64,63 @@ pCharacteristic->setValue("Hello World");
 pService->start();
 BLEAdvertising *pAdvertising = pServer->getAdvertising();
 pAdvertising->start();
+
+//Set up accelarometer
+Wire.begin();
+  delay(10);
+  if( myIMU.begin() )
+    Serial.println("Ready.");
+  else { 
+    Serial.println("Could not connect to IMU.");
+    Serial.println("Freezing");
+  }
+
+  if( myIMU.initialize(BASIC_SETTINGS) )
+    Serial.println("Loaded Settings.");
+
+//calibrate phase
+float xMax = -std::numeric_limits<float>::infinity();
+float yMax = -std::numeric_limits<float>::infinity();
+Serial.println("Calibrating...");
+for (int i = 0; i < 10; i++){
+  x = myIMU.readFloatAccelX();
+  y = myIMU.readFloatAccelY();
+  if (x > xMax){
+    xMax = x;
+      }
+  if (y > yMax){
+    yMax = y;
+      }
+  delay(1000);
+
+}
+threshold = sqrt(pow(xMax, 2) + pow(yMax, 2));
+Serial.println(threshold);
 }
 void loop() {
-  pCharacteristic->setValue("Hello World");
-  pCharacteristic->notify();
+  // 
+  // pCharacteristic->notify();
+  x = myIMU.readFloatAccelX();
+  y = myIMU.readFloatAccelY();
 
-  delay(2000);
+  // Serial.print("\nAccelerometer:\n");
+  // Serial.print(" X = ");
+
+  // Serial.println(x, 3);
+  // Serial.print(" Y = ");
+  // Serial.println(y, 3);
+
+  float rms = sqrt(pow(x, 2) + pow(y, 2));
+
+  if (rms >= threshold){
+    steps++;
+    char stepString[20];
+    snprintf(stepString, sizeof(stepString), "steps: %d", steps);
+
+    pCharacteristic->setValue((uint8_t*)stepString, strlen(stepString));
+    Serial.println(stepString);
+    
+  }
+  
+  delay(1000);
 }
