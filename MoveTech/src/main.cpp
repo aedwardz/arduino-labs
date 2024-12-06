e2182a2394bbf46851651149aca7108f5bc4e0ba
Ijeoma Arisa <iarisa@uci.edu>
2024-12-05 16:40:34 -0800

    MoveTech Setup

---
+++ MoveTech/src/main.cpp
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+#include <HttpClient.h>
+#include <WiFi.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include "esp_system.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs.h"
+#include "nvs_flash.h"
+#include <SparkFun_STHS34PF80_Arduino_Library.h>
+
+// put function declarations here:
+int myFunction(int, int);
+
+void setup() {
+  // put your setup code here, to run once:
+  int result = myFunction(2, 3);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
