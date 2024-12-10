0610795185b8da9b9bfc9342c0df83d14eda9450
aedwardz <antonile@uci.edu>
2024-12-10 15:46:59 -0800

    basic function

--- MoveTech/src/main.cpp
+++ MoveTech/src/main.cpp
@@ -10,16 +10,94 @@
 #include "nvs_flash.h"
 #include <SparkFun_STHS34PF80_Arduino_Library.h>
 
+STHS34PF80_I2C mySensor;
+
+// Values to fill with presence and motion data
+int16_t presenceVal = 0;
+int16_t motionVal = 0;
+float temperatureVal = 0;
+int ledPin1 = 26;
+int ledPin2 = 25;
+int ledPin3 = 33;
+int ledPin4 = 32;
+int ledPin5 = 27;
+
+int threshold = 500;
+
+
+
 // put function declarations here:
 int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
+  pinMode(ledPin4, OUTPUT);
+  pinMode(ledPin5, OUTPUT);
+  Serial.begin(9600);
+    Serial.println("STHS34PF80 Example 1: Basic Readings");
+
+    // Begin I2C
+    if(Wire.begin() == 0)
+    {
+      Serial.println("I2C Error - check I2C Address");
+      while(1);
+    }
+
+    // Establish communication with device 
+    if(mySensor.begin() == false)
+    {
+      Serial.println("Error setting up device - please check wiring.");
+      while(1);
+    }
+
+    delay(1000);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  sths34pf80_tmos_drdy_status_t dataReady;
+  mySensor.getDataReady(&dataReady);
+
+  // Check whether sensor has new data - run through loop if data is ready
+  if(dataReady.drdy == 1)
+  {
+    sths34pf80_tmos_func_status_t status;
+    mySensor.getStatus(&status);
+    
+    // If presence flag is high, then print data
+    if(status.pres_flag == 1)
+    {
+      // Presence Units: cm^-1
+      mySensor.getPresenceValue(&presenceVal);
+      Serial.print("Presence: ");
+      Serial.print(presenceVal);
+      Serial.println(" cm^-1");
+      if (presenceVal > threshold){
+        digitalWrite(ledPin1, HIGH);
+        digitalWrite(ledPin2, HIGH);
+        digitalWrite(ledPin3, HIGH);
+        digitalWrite(ledPin4, HIGH);
+        digitalWrite(ledPin5, HIGH);
+      }
+      else{
+        digitalWrite(ledPin1, LOW);
+        digitalWrite(ledPin2, LOW);
+        digitalWrite(ledPin3, LOW);
+        digitalWrite(ledPin4, LOW);
+        digitalWrite(ledPin5, LOW);
+      }
+      
+    }
+    else{
+      presenceVal = 0;
+    }
+
+
+    
+  }
+
 }
 
 // put function definitions here:
